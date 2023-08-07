0ba4130fe8237e80830d42f04a50fd14b7e480eb
sivakumar3442 <sivakumar.sf3442@gmail.com>
2023-08-07 19:14:30 +0530

    sample committed

---
+++ ASPCore-FileManager-WebAPI/ASPCore-FileManager-WebAPI/Controllers/FileManagerController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
+using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
+using Syncfusion.EJ2.FileManager.Base;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+
+namespace ASPCore_FileManager_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class FileManagerController : Controller
+    {
+        PhysicalFileProvider operation;
+        string basePath;
+        string root = "wwwroot\\Files";
+
+        [Obsolete]
+        public FileManagerController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            //this.basePath = "wwwroot";
+
+            this.operation = new PhysicalFileProvider();
+            if (this.basePath.EndsWith("\\"))
+                this.operation.RootFolder(this.basePath + this.root);
+            else
+                this.operation.RootFolder(this.basePath + "\\" + this.root);
+        }
+        //Validate the requested response using assigned role value
+        //[HttpPost]
+        [Route("FileOperations")]
+        [Authorize(Roles = "Admin")]
+        public object FileOperations([FromBody] FileManagerDirectoryContent args)
+        {
+            if (args.Action == "delete" || args.Action == "rename")
+            {
+                if ((args.TargetPath == null) && (args.Path == ""))
+                {
+                    FileManagerResponse response = new FileManagerResponse();
+                    response.Error = new ErrorDetails { Code = "401", Message = "Restricted to modify the root folder." };
+                    return this.operation.ToCamelCase(response);
+                }
+            }
+            switch (args.Action)
+            {
+                case "read":
+                    // reads the file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "delete":
+                    // deletes the selected file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names));
+                case "copy":
+                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "move":
+                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "details":
+                    // gets the details of the selected file(s) or folder(s).
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
+                case "create":
+                    // creates a new folder in a given path.
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
+                case "search":
+                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                case "rename":
+                    // renames a file or folder.
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName));
+                case "filter":
+                    if (args.Data[0].SearchString == "")
+                    {
+                        // Perform read operation while search string is empty.
+                        return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                    }
+                    else
+                    {
+                        // Perform Search operation while serach string has value.
+                        args.SearchString = args.Data[0].SearchString + "*";
+                        return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                    }
+            }
+            return null;
+        }
+        /// <summary>
+        /// uploads the file(s) into a specified path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="uploadFiles"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        [Route("Upload")]
+        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
+        {
+            FileManagerResponse uploadResponse;
+            foreach (var file in uploadFiles)
+            {
+                var folders = (file.FileName).Split('/');
+                // checking the folder upload
+                if (folders.Length > 1)
+                {
+                    for (var i = 0; i < folders.Length - 1; i++)
+                    {
+                        string newDirectoryPath = Path.Combine(this.basePath + path, folders[i]);
+                        if (!Directory.Exists(newDirectoryPath))
+                        {
+                            this.operation.ToCamelCase(this.operation.Create(path, folders[i]));
+                        }
+                        path += folders[i] + "/";
+                    }
+                }
+            }
+            uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+        /// <summary>
+        /// downloads the selected file(s) and folder(s)
+        /// </summary>
+        /// <param name="downloadInput"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        [Route("Download")]
+        public IActionResult Download(string downloadInput)
+        {
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            return operation.Download(args.Path, args.Names, args.Data);
+        }
+
+        /// <summary>
+        ///  gets the image(s) from the given path
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        //[HttpGet]
+        [Route("GetImage")]
+        public IActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+    }
+    public class CustomParams
+    {
+        public string FileName { get; set; }
+        public string Action { get; set; }
+    }
+}
---
+++ ASPCore-FileManager-WebAPI/ASPCore-FileManager-WebAPI/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.UseCors("AllowAllOrigins");
+app.UseEndpoints(endPoints =>
+{
+    endPoints.MapControllers();
+});
+app.Run();
